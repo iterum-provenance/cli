ca8cabfbacda00678a6ac0a9b67f0ced07cb41b5
Mantsje <edserapperloo@hotmail.com>
2020-04-29 12:46:00 +0200

    remove existence checking of the LocalCredentials path of IDV

--- idv/ctl/storage/credentials/local.go
+++ idv/ctl/storage/credentials/local.go
@@ -1,11 +1,5 @@
 package credentials
 
-import (
-	"fmt"
-
-	"github.com/iterum-provenance/iterum-go/util"
-)
-
 // Local holds required credentials for storing data locally
 type Local struct {
 	Path string `yaml:"path" json:"path"` // Absolute path to folder
@@ -13,10 +7,7 @@ type Local struct {
 
 // IsValid checks the validity of this structure
 func (l Local) IsValid() error {
-	if util.DirExists(l.Path) {
-		return nil
-	}
-	return fmt.Errorf("Error: %v is not an existing directory", l.Path)
+	return nil
 }
 
 // ParseLocal tries to parse an interface as this credential storage
